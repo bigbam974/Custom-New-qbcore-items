8d8ab2d7ff50374b44d96e077c558ce148c3c009
YRNBAM <145809841+bigbam974@users.noreply.github.com>
2024-10-01 17:51:39 -0400

    Add files via upload

---
+++ custom-foods/install.lua
@@ -0,0 +1,87 @@
+this is to just getr them into the server you have to make a shop accordingly to the script your using
+
+join discords for support
+
+
+
+-------------------------------QB/QB-Core/Shared/Items-----------------------------------
+ 
+ 
+ -----Customstorestuff
+ mtndew                    = { name = 'mtndew', 		label = 'Mtn Dew', 	 weight = 50, type = 'item', image = 'mtndew.png', 	unique = false, useable = true, shouldClose = true, description = 'Dew From A Mountain' },
+ sprite                    = { name = 'sprite', 		label = 'Sprite', 	 weight = 50, type = 'item', image = 'sprite.png', 	unique = false, useable = true, shouldClose = true, description = 'Whyys is spiccyy' },
+ monster                   = { name = 'monster', 	label = 'Monster', 	 weight = 50, type = 'item', image = 'monster.png',	unique = false, useable = true, shouldClose = true, description = 'Spike Your Energy' },
+ redbull                   = { name = 'redbull', 	label = 'Redbull', 	 weight = 50, type = 'item', image = 'redbull.png', 	unique = false, useable = true, shouldClose = true, description = 'Spike Your Energy' },
+ arizona                   = { name = 'arizona', 	label = 'Arizona', 	 weight = 50, type = 'item', image = 'arizona.png', 	unique = false, useable = true, shouldClose = true, description = 'For all the thirsty out there' },
+ minutemaid                = { name = 'minutemaid', 	label = 'MinuteMaid',	 weight = 50, type = 'item', image = 'minutemaid.png', 	unique = false, useable = true, shouldClose = true, description = 'For all the thirsty out there' },
+ drpepper                  = { name = 'drpepper', 	label = 'DrPepper', 	 weight = 50, type = 'item', image = 'drpepper.png', 	unique = false, useable = true, shouldClose = true, description = 'Pepper DR' },
+ coke                      = { name = 'coke', 		label = 'Cola', 	 weight = 50, type = 'item', image = 'cola.png', 	unique = false, useable = true, shouldClose = true, description = 'Cokey Cola' },
+ fanta                     = { name = 'fanta', 		label = 'Fanta', 	 weight = 50, type = 'item', image = 'fanta.png', 	unique = false, useable = true, shouldClose = true, description = 'Pourin The Fanta' },
+ gatorade                  = { name = 'gatorade', 	label = 'Blue Gatorade', weight = 50, type = 'item', image = 'gatorade.png', 	unique = false, useable = true, shouldClose = true, description = 'Thirsty hoe heres some aid for yo gator' },
+ gummy_worms                = { name = 'gummy_worms', 	label = 'Gummy Worms', 	 weight = 200, type = 'item', image = 'gummyworms.png', unique = false, useable = true, shouldClose = true, description = 'Worms for your stomach' },
+ skittles                   = { name = 'skittles', 	label = 'Skittles', 	 weight = 200, type = 'item', image = 'skittles.png', 	unique = false, useable = true, shouldClose = true, description = 'Taste the rainbow' },
+ snickers                   = { name = 'snickers', 	label = 'Snickers', 	 weight = 200, type = 'item', image = 'snickers.png', 	unique = false, useable = true, shouldClose = true, description = 'Your not your self' },
+ starburst                  = { name = 'starburst', 	label = 'Starburst', 	 weight = 200, type = 'item', image = 'starburst.png', 	unique = false, useable = true, shouldClose = true, description = 'Burst into the stars' },
+ slimjim                    = { name = 'slimjim', 	label = 'SlimJim', 	 weight = 200, type = 'item', image = 'slimjim.png', 	unique = false, useable = true, shouldClose = true, description = 'Snap into a slimjim' },
+ ribs                       = { name = 'ribs', 		label = 'Ribs', 	 weight = 200, type = 'item', image = 'ribs.png', 	unique = false, useable = true, shouldClose = true, description = 'Finger licking good' },
+ cheetos                    = { name = 'cheetos', 	label = 'Hot Cheetos', 	 weight = 500, type = 'item', image = 'hotcheetos.png', unique = false, useable = true, shouldClose = true, description = 'them cheetos good af' },                    
+ ramen_noodles              = { name = 'Ramen_noodles', 	label = 'Ramen Noodles', weight = 200, type = 'item', image = 'noodles.png', 	unique = false, useable = true, shouldClose = true, description = 'Aint nun like that ramen' },
+ donut_pack                 = { name = 'donut_pack', 	label = 'Pack of Donuts', weight = 200, type = 'item', image = 'donuts.png', 	unique = false, useable = true, shouldClose = true, description = 'Pack of Donuts' },
+ bbq_chips                  = { name = 'bbq_chips', 	label = 'BBQ Chips', 	 weight = 200, type = 'item', image = 'bbqchips.png', 	unique = false, useable = true, shouldClose = true, description = 'Bag of BBQ Chips' },
+ doritos                    = { name = 'doritos', 	label = 'Doritos', 	 weight = 200, type = 'item', image = 'doritos.png', 	unique = false, useable = true, shouldClose = true, description = 'Bag of Doritos' },
+ cheezits                   = { name = 'cheezits', 	label = 'CheezIts', 	 weight = 200, type = 'item', image = 'cheezits.png', 	unique = false, useable = true, shouldClose = true, description = 'Pack of Cheezitz' },
+ takis                      = { name = 'takis', 		label = 'Takis', 	 weight = 200, type = 'item', image = 'takis.png', 	unique = false, useable = true, shouldClose = true, description = 'Hot ass Takis' },
+ oreos                      = { name = 'oreos', 		label = 'Oreos', 	 weight = 200, type = 'item', image = 'oreos.png', 	unique = false, useable = true, shouldClose = true, description = 'Pack of Oreos' },
+ grandmas_cookies           = { name = 'grandmas_cookies', label = 'Grandma Cookies', weight = 200, type = 'item', image = 'gcookies.png', unique = false, useable = true, shouldClose = true, description = 'Old town Grandmas Cookies' },
+ funyuns                    = { name = 'funyuns', 	label = 'Funyuns', 	 weight = 200, type = 'item', image = 'funyuns.png', 	unique = false, useable = true, shouldClose = true, description = 'Bag of funyuns' },
+ porkskins                  = { name = 'porkskins', 	label = 'ProkSkins', 	 weight = 200, type = 'item', image = 'porkskins.png', 	unique = false, useable = true, shouldClose = true, description = 'Bag of Pork Skins' },
+
+
+ --------------------------QB/QB-Smallresources/Config-------------------------------
+
+ -- drinks
+        ['mtndew'] = math.random(40, 50),
+		['sprite'] = math.random(40, 50),
+		['monster'] = math.random(40, 50),
+		['redbull'] = math.random(40, 50),
+		['arizona'] = math.random(40, 50),
+		['minutemaid'] = math.random(40, 50),
+		['drpepper'] = math.random(40, 50),
+		['coke'] = math.random(40, 50),
+		['fanta'] = math.random(40, 50),
+		['gatorade'] = math.random(40, 50),
+
+-- food
+        ['gummy_worms'] = math.random(40, 54),
+		['skittles'] = math.random(40, 54),
+		['snickers'] = math.random(40, 54),
+		['starburst'] = math.random(40, 54),
+		['slimjim'] = math.random(40, 54),
+		['ribs'] = math.random(40, 54),
+		['cheetos'] = math.random(40, 54),
+		['Ramen_noodles'] = math.random(40, 54),
+		['donut_pack'] = math.random(40, 54),
+		['bbq_chips '] = math.random(40, 54),
+		['takis'] = math.random(40, 54),
+		['oreos'] = math.random(40, 54),
+		['grandmas_cookies'] = math.random(40, 54),
+		['funyuns'] = math.random(40, 54),
+		['porkskins'] = math.random(40, 54),
+
+------------------ADD IMAGES TO QB/QB-inventory/HTML/Images--------------------------
+
+
+Enjoyyyyyyyyy
+
+___YRNBAM
+
+join my discords
+
+--Cars&Things
+https://discord.gg/h3deXeGcBC
+
+--SDRP.V1
+https://discord.gg/f5BR4AVBev
+
+--Westpalm100kordie
+https://discord.gg/f5BR4AVBev
