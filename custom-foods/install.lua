620ef951e6a6daa5592754100bd7055fed81995c
YRNBAM <145809841+bigbam974@users.noreply.github.com>
2024-10-01 18:00:01 -0400

    Update install.lua

--- custom-foods/install.lua
+++ custom-foods/install.lua
@@ -1,4 +1,4 @@
-this is to just getr them into the server you have to make a shop accordingly to the script your using
+this is to just get them into the server you have to make a shop accordingly to the script your using
 
 join discords for support
 
